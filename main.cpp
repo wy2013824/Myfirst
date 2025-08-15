7ba27b2c96f6b90aad18cc0ba01e1e08c8feb3d6
wy2013824 <13612402913@163.com>
2025-08-15 14:22:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 
+
+#include "shoot.hpp"
+
+int main()
+{
+    initscr();
+    resize_term(32,50);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    curs_set(0);
+    noecho();
+    wall();
+    tank t;
+    t.create();
+    player p(10,10,'A');
+    p.draw();
+    while (true) {
+        p.moveplayer();
+        t.moveit();
+        t.shoot();
+        t.shooted(p);
+        t.autoshoot(p);
+        p.autoshoot();
+
+        mvprintw(31,0,"Blood: %d",t.blood);
+    }
+
+    getch();
+    endwin();
+
+    return 0;
+}
