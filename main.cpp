97da3c3d927eecebd966286467140711f9fd0a33
wy2013824 <13612402913@163.com>
2025-11-09 17:43:20 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,41 @@
 
-
+extern int score;
+extern int num;
 #include "shoot.hpp"
 
 int main()
 {
     initscr();
-    resize_term(32,50);
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);
-    curs_set(0);
+    cbreak();
     noecho();
-    wall();
-    tank t;
-    t.create();
-    player p(10,10,'A');
-    p.draw();
-    while (true) {
-        p.moveplayer();
-        t.moveit();
-        t.shoot();
-        t.shooted(p);
-        t.autoshoot(p);
-        p.autoshoot();
+    start_color();
+    curs_set(0);
 
-        mvprintw(31,0,"Blood: %d",t.blood);
+    keypad(stdscr, true);
+    nodelay(stdscr, true);
+
+
+    Player p(up,'A',25,15);
+    Enemy e;
+
+    p.draw();
+    wall();
+
+    while (true) {
+        p.moveit();
+        p.draw();
+        e.create();
+        e.moveit();
+        e.attack(p);
+        e.shooted(p);
+
+        p.autoshoot();
+        mvprintw(WIDTH+1, 0, "Score: %d/%d\tLevel: %d", score,num*16,num);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
     getch();
     endwin();
-
     return 0;
 }
